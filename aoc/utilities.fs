431a020f75483fc20d76fd14d4ec25e88654228c
Roald87 <roaldruiter@gmail.com>
2021-12-08 21:23:54 +0100

    added some string functions

--- aoc/utilities.fs
+++ aoc/utilities.fs
@@ -1,5 +1,18 @@
 namespace aoc
 
+open System.IO
+
 module Utilities =
 
     let split (sep: string) (str: string) = str.Split(sep) |> Array.toList
+
+    let parseCsv =
+        File.ReadLines
+        >> Seq.toList
+        >> List.map (split ",")
+
+    let parseSingleLineCsv = parseCsv >> List.head
+
+    let parseSingleLineIntCsv = parseSingleLineCsv >> List.map int
+
+    let stringToChars = Seq.toArray
